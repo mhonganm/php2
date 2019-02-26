a7b572b30424d926fd11ba532ba24ae1495c583f
Mikko Honganmäki <42143818+mhonganm@users.noreply.github.com>
2019-02-26 19:41:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,5 @@
 <body>
 <?php echo '<h1>Heippa maailma</h1>'; ?>
 <?php echo '<p>mitä kuuluu</p>'; ?>
-<?php echo '<p>tämän pitäs nyt laukasta uusi versio</p>'; ?>
-<?php echo '<p>ja jälleen uusi versio</p>'; ?>
-<?php echo '<p>Jenkins demo</p>'; ?>
-<?php echo '<p>Lisätty rivi 25.9</p>'; ?>
-<?php echo '<p>Juhan Demo</p>'; ?>
-<?php echo '<p>Juhan Demo2</p>'; ?>
-<?php echo '<p>Juhan Demo3</p>'; ?>
-<?php echo '<p>nopea testi2 /mikko</p>'; ?>
-<?php echo '<p>nopea testi2 /mikko</p>'; ?>
-<?php echo '<p>nopea testi3 /mikko</p>'; ?>
 </body>
 </html>
